6c06a9170813abadc3346c28980acdfeff144582
Simon peter <petersimon155@gmail.com>
2021-04-13 12:21:53 +0600

    Added test for properties in commmand model

--- test/TrailAPI.Tests/CommandModeltest.cs
+++ test/TrailAPI.Tests/CommandModeltest.cs
@@ -1,26 +1,57 @@
+using System;
 using TrailAPI.Models;
 using Xunit;
 
 namespace TrailAPI.Tests
 {
-    public class CommandModelTest
+    public class CommandModelTest: IDisposable
     {
+         CommandModel testCommand;
+         
+        public CommandModelTest(){
+             //Arrange
+            testCommand = new CommandModel
+            {
+             HowTo= "Do something awesome",
+             Platform= "Some platform",
+             CommandLine="Some commandline"
+            };
+        }
+
+        public void Dispose(){
+            testCommand=null;
+        }
+
         [Fact]
         public void CanChangeHowTo()
         {
-            //Arrange
-            var testCommand = new CommandModel
-            {
-             HowTo= "Do something awesome",
-             Platform= "xUnit",
-             CommandLine="dotnet test"
-            };
-
+        
             //act
             testCommand.HowTo="Execute Unit Tests";
 
             //Assert
             Assert.Equal("Execute Unit Tests",testCommand.HowTo);
+        }
+
+        [Fact]
+        public void CanChangePlatform(){
+              //act
+            testCommand.Platform="xUnit";
+
+            //Assert
+            Assert.Equal("xUnit",testCommand.Platform);
+
+        }
+
+        
+        [Fact]
+        public void CanChangeCommandLine(){
+              //act
+            testCommand.CommandLine="dotnet test";
+
+            //Assert
+            Assert.Equal("dotnet test",testCommand.CommandLine);
+
         }
 
     }
